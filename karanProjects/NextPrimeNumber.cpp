b7bda1fa3c02f9c099461601c84f509c5f672e7c
Adam Hock <35805229+ahock2@users.noreply.github.com>
2018-02-06 11:55:59 -0600

    Add files via upload

---
+++ karanProjects/NextPrimeNumber.cpp
@@ -0,0 +1,52 @@
+/*
+ 
+ Name: NextPrimeNumber.cpp
+ 
+ Creator: Adam Hock
+ 
+ Date started: 11/15/2017
+ 
+ Description: This program accepts a numerical user input and outputs the next 
+ prime number after the user's inputted number.
+ 
+ Comments: This project was inspired by github user "karan" who created a "Mega 
+ Project List" that features many projects that coders can work on for practice.
+ The list for her projects can be found in the following link. 
+ https://github.com/karan/Projects. 
+  
+ This program can be updated to tell the user the next composite number, tell
+ the user if their inputted number is prime or composite, and maybe show factors
+ of a composite number to prove the number is in fact composite.
+ 
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    int input;
+    int NextPrime;
+    cout << "Please enter an integer" << endl;
+    cin >> input;
+    int i = input + 1;
+    while(true)
+    {
+        NextPrime = 1;
+        for (int j = 2; j < i; j++)
+        {
+            if (i%j == 0)
+                NextPrime=0;
+        }
+        if (NextPrime == 1)
+        {
+            cout << "The next prime number is " << i << endl;
+            break;
+        }
+        i++;
+    }
+    
+    return 0;
+}
+
